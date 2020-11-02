5282ec78679127c7f23e4d0f37643d2a1f4d2b72
Gabriel Rodrigues <gabrirodri123@gmail.com>
2020-11-02 01:52:18 -0200

    Payment, one-to-one association

--- src/main/java/com/crujava/curso/config/TestConfig.java
+++ src/main/java/com/crujava/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.crujava.curso.entities.Category;
 import com.crujava.curso.entities.Order;
 import com.crujava.curso.entities.OrderItem;
+import com.crujava.curso.entities.Payment;
 import com.crujava.curso.entities.Product;
 import com.crujava.curso.entities.User;
 import com.crujava.curso.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRespositoy.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+	
+		orderRepository.save(o1);
+	
 	}
 	
 }
